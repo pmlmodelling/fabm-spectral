62340397810dc6107ae91beac73e69a26a4511d8
Jorn Bruggeman <jbr@pml.ac.uk>
2018-09-11 17:31:53 +0100

    added Slingo 1989 cloud model

---
+++ data/oasim/parse_slingo.py
@@ -0,0 +1,30 @@
+lambda_mins, lambda_maxs, a_s, bs, es, fs, cs, ds = [], [], [], [], [], [], [], []
+with open('slingo.dat', 'rU') as f:
+    for i in range(3):
+        f.readline()
+    for l in f:
+        items = l.rstrip('\n').split()
+        lambda_min, lambda_max, a, b, e, f, c, d = map(float, items)
+        lambda_mins.append(lambda_min)
+        lambda_maxs.append(lambda_max)
+        a_s.append(a)
+        bs.append(b)
+        cs.append(c)
+        ds.append(d)
+        es.append(e)
+        fs.append(f)
+n = len(lambda_mins)
+
+def write(f, arr, name):
+    f.write('real(rk), parameter :: %s(%i) = (/%s/)\n' % (name, len(arr), ', '.join(['%g_rk' % v for v in arr])))
+
+with open('../../src/slingo_const.inc', 'w') as f:
+    f.write('real(rk), parameter :: nslingo = %i\n' % len(lambda_mins))
+    write(f, lambda_mins, 'lambda_min')
+    write(f, lambda_maxs, 'lambda_max')
+    write(f, a_s, 'slingo_a')
+    write(f, bs, 'slingo_b')
+    write(f, cs, 'slingo_c')
+    write(f, ds, 'slingo_d')
+    write(f, es, 'slingo_e')
+    write(f, fs, 'slingo_f')
---
+++ src/slingo.F90
@@ -0,0 +1,69 @@
+module slingo_clouds
+
+   use fabm_types, only: rk
+
+   implicit none
+
+#include "slingo_const.inc"
+
+contains
+   
+   subroutine slingo(LWP, r_e, mu0)
+      ! LWP: liquid water path (g m-2)
+      ! r_e: equivalent radius of drop size distribution (um)
+      ! mu0: consine of zenith angle
+      real(rk), intent(in) :: LWP, r_e, mu0
+
+      real(rk), dimension(nslingo) :: tau, omega, g
+      real(rk), dimension(nslingo) :: beta0, beta_mu0, f, U2
+      real(rk), dimension(nslingo) :: alpha1, alpha2, alpha3, alpha4, epsilon, M, E, gamma1, gamma2
+      real(rk), dimension(nslingo) :: T_DB, R_DIF, T_DIF, R_DIR, T_DIR
+      real(rk) :: U1
+
+      ! Slingo 1989 Eqs 1-3
+      ! cloud optical depth, single scatter albedo, asymmetry parameter
+      ! derived for r_e = [4.2, 16.6]
+      tau = LWP * (slingo_a + slingo_b / r_e)
+      omega = -(slingo_c + slingo_d * r_e - 1)
+      g = slingo_e + slingo_f * r_e
+
+      ! fraction of scattered diffuse radation which is scattered into the backward hemisphere (Slingo 1989 Eq 6)
+      beta0 = 3._rk / 7._rk * (1 - g)
+
+      ! fraction of scattered direct radation which is scattered into the backward hemisphere (Slingo 1989 Eq 7)
+      beta_mu0 = 0.5_rk - 0.75_rk * mu0 * g / (1 + g)
+
+      ! fraction of scattered direct flux which emerges at zenith angles close to that of the incident beam (Slingo 1989 Eq 8)
+      f = g * g
+
+      ! reciprocals of the effective cosine for diffuse upward and downward fluxes (Slingo 1989 Eqs 9 and 10)
+      U1 = 7._rk / 4._rk
+      U2 = 7._rk / 4._rk * (1._rk - (1 - omega) / 7._rk / omega / beta0)
+
+      alpha1 = U1 * (1 - omega * (1 - beta0))
+      alpha2 = U2 * omega * beta0
+      alpha3 = (1 - f) * omega * beta_mu0
+      alpha4 = (1 - f) * omega * (1 - beta_mu0)
+      epsilon = sqrt(alpha1 * alpha1 - alpha2 * alpha2)
+      M = alpha2 / (alpha1 + epsilon)
+      E = exp(-epsilon * tau)
+      gamma1 = (1 - omega * f) * alpha3 - mu0 * (alpha1 * alpha3 + alpha2 * alpha4) / ((1 - omega * f)**2 - epsilon**2 * mu0**2)
+      gamma2 = -(1 - omega * f) * alpha4 - mu0 * (alpha1 * alpha4 + alpha2 * alpha3) / ((1 - omega * f)**2 - epsilon**2 * mu0**2)
+
+      ! Transmissivity for the direct solar beam (Slingo 1989 Eq 20)
+      T_DB = exp(-(1 - omega * f) * tau / mu0)
+
+      ! Diffuse reflectivity for diffuse incident radiation (Slingo 1989 Eq 21)
+      R_DIF = M * (1 - E**2) / (1 - E**2 * M**2)
+
+      ! Diffuse transmissivity for diffuse incident radiation (Slingo 1989 Eq 22)
+      T_DIF = E * (1 - M**2) / (1 - E**2 * M**2)
+
+      ! Diffuse reflectivity for direct incident radiation (Slingo 1989 Eq 21)
+      R_DIR = -gamma2 * R_DIF - gamma1 * T_DB * T_DIF + gamma1
+
+      ! Diffuse transmissivity for direct incident radiation (Slingo 1989 Eq 21)
+      T_DIR = -gamma2 * T_DIF - gamma1 * T_DB * R_DIF + gamma2 * T_DB
+   end subroutine
+
+   end module slingo_clouds
