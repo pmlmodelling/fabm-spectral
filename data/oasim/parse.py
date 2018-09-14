1993db02d8d16f7cf14e05568310af9298dd772d
Jorn Bruggeman <jbr@pml.ac.uk>
2018-09-14 16:33:29 +0100

    correctly scale slingo constants

--- data/oasim/parse.py
+++ data/oasim/parse.py
@@ -23,7 +23,11 @@ lam, a, b = parse('abw25b.dat', 6, range(3))
 write('../../src/water_const.inc', nlambda_w=len(lam), lambda_w=lam, a_w=a, b_w=b)
 
 lambda_min, lambda_max, a, b, e, f, c, d = parse('slingo.dat', 3, range(8))
-write('../../src/slingo_const.inc', nlambda_slingo=len(lambda_min), lambda_min_slingo=lambda_min*1000, lambda_max_slingo=lambda_max*1000, lambda_slingo=(lambda_min + lambda_max)*500, a_slingo=a, b_slingo=b, c_slingo=c, d_slingo=d, e_slingo=e, f_slingo=f)
+a *= 0.01
+f *= 0.001
+lambda_min *= 1000
+lambda_max *= 1000
+write('../../src/slingo_const.inc', nlambda_slingo=len(lambda_min), lambda_min_slingo=lambda_min, lambda_max_slingo=lambda_max, lambda_slingo=(lambda_min + lambda_max)*0.5, a_slingo=a, b_slingo=b, c_slingo=c, d_slingo=d, e_slingo=e, f_slingo=f)
 
 lam, ET, a_r, a_o, a_v, a_o2, a_co2 = parse('atmo25b.dat', 4, range(7))
 write('../../src/oasim_const.inc', nlambda_oasim=len(lam), lambda_oasim=lam, ET_oasim=ET, a_r_oasim=a_r, a_o_oasim=a_o, a_v_oasim=a_v, a_u_oasim=a_o2 + a_co2)
